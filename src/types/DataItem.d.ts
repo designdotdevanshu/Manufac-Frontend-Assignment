e6c9ef413a7f98b3070adc72e912f18ec817bcff
Devanshu <design.devanshu@gmail.com>
2024-07-21 19:40:11 +0530

    feat: Add DataItem type definition

---
+++ src/types/DataItem.d.ts
@@ -0,0 +1,7 @@
+export type DataItem = {
+  Year: string;
+  "Crop Name": string;
+  "Crop Production (UOM:t(Tonnes))": string;
+  "Yield Of Crops (UOM:Kg/Ha(KilogramperHectare))": string;
+  "Area Under Cultivation (UOM:Ha(Hectares))": string;
+};
